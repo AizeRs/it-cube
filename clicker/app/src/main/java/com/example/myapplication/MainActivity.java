5f2a3db0f440efe2fcca3ca9662f839939b75e53
AizeRs <NAKorshunov@mail.ru>
2019-11-18 20:16:00 +0500

    Intent (1st try)
    1st try to do Intent.

--- clicker/app/src/main/java/com/example/myapplication/MainActivity.java
+++ clicker/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Intent intent = new Intent(this, MainActivity.class);
+//    Intent GetIntent = getIntent();
+//    Integer a = intent.getIntExtra("a", 0);
+//    Integer koef = intent.getIntExtra("koef", 1);
+//    Integer chance = intent.getIntExtra("chance", 3);
+//    Integer chance2 = intent.getIntExtra("chance2", 2);
+
   private String LOG = "buy";
-  private int a = 0;
-  private int koef = 1;
-  private int price = 10;
-  private int price2 = 20;
-  private int chance = 3;
-  private int chance2 = 2;
+  public int a = 0;
+  public int koef = 1;
+  public int chance = 3;
+  public int chance2 = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,36 +33,15 @@ public class MainActivity extends AppCompatActivity {
         a += koef;
         text.setText("Your $: " + a);
     }
-    public void onClickButton25(View view){
-        Log.i(LOG, "клик");
-        TextView koefout = (TextView)findViewById(R.id.koefout);
-        TextView koefread = (TextView)findViewById(R.id.textView2);
-        TextView text = (TextView)findViewById(R.id.textt);
-        if(a >= price){
-            int b = Chance.get(chance);
-            a -= price;
-            koef += b;
-            price += 10;
-            koefout.setText("price 1: " + price);
-            koefread.setText("$ per click: " + koef);
-            text.setText("Your $: " + a);
-        }
+    public void onClickButtonShop(View view){
+        Intent intent = new Intent(this, ShopActivity.class);
+        intent.putExtra("a", a);
+        intent.putExtra("koef", koef);
+        intent.putExtra("chance", chance);
+        intent.putExtra("chance2", chance2);
+        startActivity(intent);
     }
-    public void onClickButton50(View view) {
-        Log.i(LOG, "клик");
-        TextView koefout = (TextView) findViewById(R.id.koefout2);
-        TextView koefread = (TextView) findViewById(R.id.textView2);
-        TextView text = (TextView) findViewById(R.id.textt);
-        if (a >= price2) {
-            int b = Chance.get(chance2);
-            a -= price2;
-            koef += b;
-            price2 += 20;
-            koefout.setText("price 2: " + price2);
-            koefread.setText("$ per click: " + koef);
-            text.setText("Your $: " + a);
-        }
-    }
+
     public void onClickButtonADM(View view){
         chance = 1;
         chance2 = 1;
