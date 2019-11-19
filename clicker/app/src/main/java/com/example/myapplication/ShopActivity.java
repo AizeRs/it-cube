7e90c1e9302d695008260186a992ec0c955770d7
AizeRs <55633813+AizeRs@users.noreply.github.com>
2019-11-19 13:27:52 +0500

    Intent try 2
    Not a big update, can be deleted.

--- clicker/app/src/main/java/com/example/myapplication/ShopActivity.java
+++ clicker/app/src/main/java/com/example/myapplication/ShopActivity.java
@@ -9,12 +9,12 @@ import android.widget.TextView;
 
 public class ShopActivity extends AppCompatActivity {
 
-    Intent intent = new Intent(this, MainActivity.class);
-    Intent GetIntent = getIntent();
-    Integer a = intent.getIntExtra("a", 0);
-    Integer koef = intent.getIntExtra("koef", 1);
-    Integer chance = intent.getIntExtra("chance", 3);
-    Integer chance2 = intent.getIntExtra("chance2", 2);
+//    Intent Gintent = new Intent(this, MainActivity.class);
+    //Intent intent = getIntent();
+    //Integer a = intent.getIntExtra("a", 0);
+    //Integer koef = intent.getIntExtra("koef", 1);
+    //Integer chance = intent.getIntExtra("chance", 3);
+    //Integer chance2 = intent.getIntExtra("chance2", 2);
 
     public int price = 10;
     public int price2 = 20;
@@ -54,10 +54,10 @@ public class ShopActivity extends AppCompatActivity {
         }
     }
     public void onClickButtonBack(View view) {
-        intent.putExtra("a", a);
-        intent.putExtra("koef", koef);
-        intent.putExtra("chance", chance);
-        intent.putExtra("chance2", chance2);
-        startActivity(intent);
+        //Gintent.putExtra("a", a);
+        //Gintent.putExtra("koef", koef);
+        //Gintent.putExtra("chance", chance);
+        //Gintent.putExtra("chance2", chance2);
+        //startActivity(Gintent);
     }
 }
